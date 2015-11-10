188944de7baf6c5abfc8bcf6b7d803bbf2bce8a0
toukkeli <toukkeli@hotmail.com>
2015-11-10 19:35:03 +0200

    Kunnollinen liike ja kamera.

---
+++ Rhythm game/Assets/InputController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+	GameObject[] notes;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown ("1")) {
+			notes = GameObject.FindGameObjectsWithTag("note1");
+
+			for (int i = 0; i < notes.Length; i++){
+
+				//TODO Tämä ei toimi
+				if ((notes[i].transform.position.x > -64) || (notes[i].transform.position.x < 64)){
+
+					print("1 key was pressed at the right time!");
+				}
+			}
+		}
+	}
+}
--- Rhythm game/Assets/NoteMovement.cs
+++ Rhythm game/Assets/NoteMovement.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class NoteMovement : MonoBehaviour {
 
+	public float XSpeed;
+	public float YSpeed;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,6 @@ public class NoteMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.position += new Vector3(0.01f,-0.01f,0);
+		transform.position += new Vector3(XSpeed,YSpeed,0);
 	}
 }
